3ef56c4fcc8e9dff08dbda4b509636c626b8c61a
AsahinaHaruka <92851255+AsahinaHaruka@users.noreply.github.com>
2025-03-05 18:57:00 +0800

    Update 明文源码.js
    add uuid

--- 明文源码.js
+++ 明文源码.js
@@ -1,7 +1,7 @@
 
 import { connect } from 'cloudflare:sockets';
 
-let userID = '';
+let userID = '241D40C6-9442-448D-93EA-54581E8BD04C';
 let proxyIP = '';
 //let sub = '';
 let subConverter = atob('U1VCQVBJLkNNTGl1c3Nzcy5uZXQ=');
